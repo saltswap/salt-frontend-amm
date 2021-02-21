fa0b8821c149f4af98936fb700e03f23d2d4d766
saltKosher <saltswapfinance@gmail.com>
2021-02-21 14:38:46 +1100

    refactor: Add SALT contract address

--- src/constants/saltPriceContracts.ts
+++ src/constants/saltPriceContracts.ts
@@ -1,5 +1,5 @@
-const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
-  cakeAddress: '0xf952fc3ca7325cc27d15885d37117676d25bfda6',
+const priceContracts: { cakeAddress: string, busdAddress: string, lpAddress: string } = {
+  cakeAddress: '0x2849b1aE7E04A3D9Bc288673A92477CF63F28aF4',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
   lpAddress: '0x19e7cbecdd23a16dfa5573df54d98f7caae03019'
 }
